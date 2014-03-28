9148e3db68bf75e33e34cec345048df8ae9b98ae
Matt Brennan <mattyb1000@gmail.com>
2014-03-28 14:43:57 +0000

    stuff lol

---
+++ index.js
@@ -0,0 +1,19 @@
+var minimatch = require('minimatch');
+var through = require('through');
+
+function replaceify(file, options) {
+  for(var glob in options) if(options.hasOwnProperty(glob)) {
+    if(minimatch(file, glob)) {
+      return through(
+        function write{ /* noop */ },
+        function end{
+          this.queue(options[glob]);
+          this.queue(null);
+        }
+      );
+    }
+  }
+  return through();
+}
+
+module.exports = replaceify;
