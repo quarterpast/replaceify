105662d0e8919b911516a24699f5703f7913af5b
Matt Brennan <mattyb1000@gmail.com>
2014-03-28 16:26:22 +0000

    glob relative to cwd

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var minimatch = require('minimatch');
 var through = require('through');
+var path = require('path');
 
 function replaceify(file, options) {
+  var relative = path.relative(process.cwd(), file);
+
   for(var glob in options) if(options.hasOwnProperty(glob)) {
-    if(minimatch(file, glob)) {
+    if(minimatch(relative, glob)) {
       return through(
         function write(){ /* noop */ },
         function end(){
